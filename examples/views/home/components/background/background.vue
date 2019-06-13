bca2a547f078ad9854cc80610764b53ed7dd87a7
penghongsheng <penghongsheng@yixinghealth.com>
2019-06-13 16:53:58 +0800

    ..

--- examples/views/home/components/background/background.vue
+++ examples/views/home/components/background/background.vue
@@ -5,14 +5,9 @@
 <script>
 import map from './js/Map'
 import Star from './js/Star'
-import ParticleLine from './js/ParticleLine'
 
 let stars = [];   // 闪烁的星星
-const starsCount = 0;      // 闪烁的星星数量
-
-let particle = [];  // 粒子线条
-const particleCount = 200;
-const maxline = 9000;    // 触发连线的距离
+const starsCount = 50;      // 闪烁的星星数量
 
 const raf = window.requestAnimationFrame
   || window.webkitRequestAnimationFrame
@@ -43,25 +38,16 @@ export default {
             for(let i = 0; i < count; i++){
                 const x = Math.random() * map.width;
                 const y = Math.random() * map.height;
-                stars.push(new Star({x, y}));
-            }
-        },
-        // 添加粒子线条
-        createParticle(count){
-            particle = [];
-            for(let i = 0; i < count; i++){
-                const x = Math.random() * map.width;
-                const y = Math.random() * map.height;
                 const xa = (Math.random() * 2 - 1)/5;
                 const ya = (Math.random() * 2 - 1)/5;
-                particle.push(new ParticleLine({x, y, xa, ya}));
+                stars.push(new Star({x, y}));
+                stars.push(new Star({x, y, xa, ya}));
             }
         },
         // 所有数据初始化
         dataInit(){
             this.backInit();
             this.createstars(starsCount);
-            this.createParticle(particleCount);
         },
         // 开始动画
         startAnimat(){
@@ -72,28 +58,6 @@ export default {
             for(let i=0; i<stars.length; i++){
                 stars[i].render();
                 stars[i].update();
-            }
-
-            // 粒子线条渲染
-            let dot;
-            let nowDot;
-            for(let i=0; i<particle.length; i++){
-                dot = particle[i];
-                dot.render();
-                dot.update();
-
-                for(let j=0; j<particle.length; j++){
-                    nowDot = particle[j];
-                    // continue跳出当前循环开始新的循环
-                    if(nowDot===dot||nowDot.x===null||nowDot.y===null) continue;
-                    // 计算两点间的距离
-                    let dx = dot.x - nowDot.x,
-                        dy = dot.y - nowDot.y;
-                    let dc = dx*dx + dy*dy;
-                    if(Math.sqrt(dc)>Math.sqrt(maxline)) continue;
-                    let ratio = (maxline - dc) / maxline;
-                    dot.line(nowDot, ratio);
-                }
             }
 
             raf(this.startAnimat);
--- examples/views/home/components/background/js/Star.js
+++ examples/views/home/components/background/js/Star.js
@@ -18,15 +18,27 @@ class Star extends Point {
         this.speed = options.speed || Math.random() * 2 + 0.5;      // 速度
         this.angle = 0;                                          // 旋转角度
         this.r = Math.random() - 0.8;                            // 旋转半径
+        this.xa = options.xa || null;                   // x偏移量
+        this.ya = options.ya || null;                   // y偏移量
     }
     // 更新方法
     update() {
         this.color += 0.1;
-        // 圆周运动
-        this.r = this.r > 0.1 ? 0.1 : (this.r < -0.1 ? -0.1 : this.r)
-        this.x += this.r*Math.cos(this.angle*(Math.PI/180));
-        this.y += this.r*Math.sin(this.angle*(Math.PI/180));
-        this.angle+=1;
+        if(this.xa && this.ya){
+            this.x += this.xa;
+            this.y += this.ya;
+
+            // 遇到边界速度反向
+            this.xa *= (this.x < -10 || this.x > (map.width + 10)) ? -1 : 1;
+            this.ya *= (this.y < -10 || this.y > (map.height + 10)) ? -1 : 1;
+        }else{
+            // 圆周运动
+            this.r = this.r > 0.1 ? 0.1 : (this.r < -0.1 ? -0.1 : this.r)
+            this.x += this.r*Math.cos(this.angle*(Math.PI/180));
+            this.y += this.r*Math.sin(this.angle*(Math.PI/180));
+            this.angle+=1;
+        }
+        
         // 放大缩小
         if(this.radiuschangType == 'add'){
             this.startradius += 0.01;
